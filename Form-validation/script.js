85249282726623ce4be8bf0a59fb12a7b19a8e6f
hhnssl <losnah5h@gmail.com>
2022-09-02 21:18:41 +0900

    (리팩토링o) 기본적인 유효성 검사 js 완료 - 주석 제거

--- Form-validation/script.js
+++ Form-validation/script.js
@@ -27,12 +27,7 @@ function isValidEmail(email) {
 // Check required fields
 function checkRequired(inputArr) {
   inputArr.forEach(function (input) {
-    // console.log(input.value);
     if (input.value.trim() === '') {
-      // trim()은 공백을 없애는 함수인데, 이게 없으면 스페이스만 쳐도 썼다고 인식됨
-      // input은 태그 전체, input.id는 태그의 id 속성
-
-      // showError(input, `${input.id} is required`); 대신에 밑에걸 사용하겠음
       showError(input, `${getFieldName(input)} is required`);
     } else {
       showSuccess(input);
@@ -40,11 +35,7 @@ function checkRequired(inputArr) {
   });
 }
 
-// Get fieldname: input.id는 username과 같이 단어의 첫번째 글자가 소문자임. 이걸 대문자로 바꾸기 위한 함수
 function getFieldName(input) {
-  // ex) username
-  // input.id.charAt(0).toUpperCase() ==> u 하나만 선택해서 대문자로 바꿈 -> U
-  // input.id.slice(1) ==> 첫번째 글자를 제외한 나머지 글자 가져옴 -> sername
   return input.id.charAt(0).toUpperCase() + input.id.slice(1);
 }
 
@@ -52,7 +43,5 @@ function getFieldName(input) {
 form.addEventListener('submit', function (event) {
   event.preventDefault();
 
-  // checkRequired(username);
-  // checkRequired(password); ... 이런식으로 함수 한번 호출에 변수 하나만 담아 보내는 게 비효율적이므로
-  checkRequired([username, email, password, password2]); // 와 같이 배열로 만들어 보내버린다. (단, checkRequired에서 사용할 때 반복문 필요)
+  checkRequired([username, email, password, password2]);
 });
