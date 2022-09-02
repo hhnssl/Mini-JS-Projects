0b3bdc6fd2935ea341dc8f57c460be2f7343b8b1
hhnssl <losnah5h@gmail.com>
2022-09-02 20:22:10 +0900

    (리팩토링x) 기본적인 유효성 검사 js 완료 - 주석 제거

--- script.js
+++ script.js
@@ -6,13 +6,11 @@ const password2 = document.getElementById('password2');
 
 // Show input error message
 function showError(input, message) {
-  const formControl = input.parentElement; // -> input의 부모태그를 formControl에 할당. (즉, form-control 이라는 클래스명을 가진 div를 formControl에 할당하겠다는 뜻)
+  const formControl = input.parentElement;
+  formControl.className = 'form-control error';
 
-  formControl.className = 'form-control error'; // <- 해당 div 클래스에 form-contorl과 error를 넣어줌.(원래는 form-control만 있었는데 error까지 들어가게끔)
-
-  const small = formControl.querySelector('small'); //해당 div 클래스의 자식에서 small 태그 선택해서 할당
-
-  small.innerText = message; // 원래 들어있던 컨텐츠를 인자로 받은 message로 갈아끼우기~
+  const small = formControl.querySelector('small');
+  small.innerText = message;
 }
 
 // Show success outline
@@ -23,8 +21,6 @@ function showSuccess(input) {
 
 // Check email is valid
 function isValidEmail(email) {
-  // js email Regex 검색해서 맘에 드는 거 가져오기
-  // 왼쪽의 정규표현식을 email과 비교하는 테스트를 해서 테스트 통과(= 올바른 양식)하면 true 반환, 아니면 false
   return /^[\w\-\.\+]+\@[a-zA-Z0-9\.\-]+\.[a-zA-z0-9]{2,5}$/.test(email);
 }
 
@@ -41,7 +37,6 @@ form.addEventListener('submit', function (event) {
   if (email.value === '') {
     showError(email, 'email is required');
   } else if (!isValidEmail(email.value)) {
-    // 유효성 검사에 실패하면(false 반환받으면) 이메일이 유효하지 않다는 매세지 띄우기
     showError(email, 'Email is not valid ! !');
   } else {
     showSuccess(email);
