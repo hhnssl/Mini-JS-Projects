7aef8aa1df617ebb39911e72b72dad9ba1384018
hhnssl <losnah5h@gmail.com>
2022-09-05 21:48:09 +0900

    새로고침해도 값이 유지되도록 변경

--- Movie-booking/script.js
+++ Movie-booking/script.js
@@ -4,6 +4,8 @@ const count = document.getElementById('count');
 const total = document.getElementById('total');
 const movieSelect = document.getElementById('movie');
 
+populateUI();
+
 let ticketPrice = +movieSelect.value;
 
 // Save selected movie index and price
@@ -28,6 +30,25 @@ function updateSelectedCount() {
   total.innerText = selectedSeatsCount * ticketPrice;
 }
 
+// Get data from localstorage and populate UI
+function populateUI() {
+  const selectedSeats = JSON.parse(localStorage.getItem('selectedSeats'));
+
+  if (selectedSeats !== null && selectedSeats.length > 0) {
+    seats.forEach((seat, index) => {
+      if (selectedSeats.indexOf(index) > -1) {
+        seat.classList.add('selected');
+      }
+    });
+  }
+
+  const selectedMovieIndex = localStorage.getItem('selectedMovieIndex');
+
+  if (selectedMovieIndex !== null) {
+    movieSelect.selectedIndex = selectedMovieIndex;
+  }
+}
+
 // Movie select event
 movieSelect.addEventListener('change', (event) => {
   ticketPrice = +event.target.value;
@@ -47,3 +68,6 @@ container.addEventListener('click', (event) => {
     updateSelectedCount();
   }
 });
+
+// Initial count and total set
+updateSelectedCount();
