073303f8225883809ce34740e40a5a206574f6d9
hhnssl <losnah5h@gmail.com>
2022-09-19 22:07:42 +0900

    영화 카드 추가 기능 구현

--- 영화예매사이트/script.js
+++ 영화예매사이트/script.js
@@ -1,9 +1,10 @@
 const API_URL =
   'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=49a69e11099bff09da12666b0cdb2d56&page=1';
-const IMG_PATH = 'https://image.tmdb.org/t/p/w51280';
+const IMG_PATH = 'https://image.tmdb.org/t/p/w1280';
 const SEARCH_API =
   'https://api.themoviedb.org/3/search/movie?api_key=49a69e11099bff09da12666b0cdb2d56&query=';
 
+const main = document.getElementById('main');
 const form = document.getElementById('form');
 const search = document.getElementById('search');
 
@@ -15,6 +16,45 @@ async function getMovies(url) {
   const data = await res.json();
 
   console.log(data.results);
+  showMovies(data.results);
+}
+
+// 영화(data.results) 가져오기
+function showMovies(movies) {
+  // main 요소에 있던 요소들 없애기
+  main.innerHTML = '';
+
+  movies.forEach((item) => {
+    const { title, poster_path, vote_average, overview } = item;
+
+    // html 파일에 만들었던 .movie요소를 아래의 코드로 대체하기
+    const movieEl = document.createElement('div'); // 빈 div 생성
+    movieEl.classList.add('movie'); // div에 클래스명 부여
+
+    movieEl.innerHTML = `
+    <img src="${IMG_PATH + poster_path}" alt="${title}" />
+
+        <div class="movie-info">
+          <h3>${title}</h3>
+          <span class="${getClassbyRate(vote_average)}">${vote_average}</span>
+        </div>
+
+        <div class="overview">
+          <h3>개요</h3>
+          ${overview}
+        </div>
+    `;
+
+    // main 요소에, 만든 div.movie 추가
+    main.appendChild(movieEl);
+  });
+}
+
+// 평점에 따라 색 변경을 위한 클래스명 부여 함수
+function getClassbyRate(vote) {
+  if (vote >= 8) return 'green';
+  if (vote >= 5) return 'orange';
+  if (vote < 5) return 'red';
 }
 
 // 검색란
@@ -33,3 +73,5 @@ form.addEventListener('submit', (e) => {
     window.location.reload();
   }
 });
+
+// https://www.hellodigital.kr/blog/dmkt-general-pagination-vs-infinite-scroll-02/
