ffff45b98759a2aedf71d76412e18375a85b9b6b
hhnssl <losnah5h@gmail.com>
2022-09-23 23:41:50 +0900

    타자연습게임 타이머 추가

--- Typing-game/script.js
+++ Typing-game/script.js
@@ -2,7 +2,7 @@ const word = document.getElementById('word');
 const text = document.getElementById('text');
 const scoreEl = document.getElementById('score');
 const timeEl = document.getElementById('time');
-const endgameEl = document.getElementById('end-game');
+const endgameEl = document.getElementById('end-game-container');
 const settingsBtn = document.getElementById('settings-btn');
 const settings = document.getElementById('settings');
 const settingsForm = document.getElementById('settings-form');
@@ -37,6 +37,12 @@ let randomWord;
 let score = 0;
 let time = 10;
 
+// 페이지가 로드되면 인풋창에 자동 포커스
+text.focus();
+
+// 카운트 시작, 1초마다 updateTime 호출
+const timeInterval = setInterval(updateTime, 1000);
+
 // 배열에서 랜덤으로 단어 가져오기
 function getRandomWord() {
   return words[Math.floor(Math.random() * words.length)];
@@ -55,6 +61,33 @@ function updateScore() {
   scoreEl.innerHTML = score;
 }
 
+// 시간 업데이트
+function updateTime() {
+  time--;
+  timeEl.innerHTML = time + 's';
+
+  // 0초가 되면
+  if (time === 0) {
+    // 내장함수로 timeInterval함수 끝내기
+    clearInterval(timeInterval);
+
+    // 게임 끝내기
+    gameOver();
+  }
+}
+
+// 게임 오버. 점수 및 재시작 버튼 띄우기
+function gameOver() {
+  endgameEl.innerHTML = `
+    <h1>시간 초과</h1>
+    <p>최종 점수: ${score}</p>
+    <button onclick="location.reload()">다시하기</button>
+  `;
+
+  // display: none으로 숨겨놨던 엔드스크린 띄우기
+  endgameEl.style.display = 'flex';
+}
+
 addWordToDOM();
 
 text.addEventListener('input', (e) => {
@@ -71,5 +104,9 @@ text.addEventListener('input', (e) => {
 
     // input창 비우기
     e.target.value = '';
+
+    // 맞출 때마다 2초 추가
+    time += 2;
+    updateTime();
   }
 });
